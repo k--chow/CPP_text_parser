31a7eb2922da7c6f79613e1922876a847f49e993
kchow23 <kchow23@ufl.edu>
2015-11-30 18:36:36 -0500

    Initial Commit

---
+++ parse.cpp
@@ -0,0 +1,10 @@
+
+//get all CAPS based on 3 capital letters
+//save all the categories to the prefix
+//save to hashmap, if multiple use linkedlist
+//also save each to array, if duplicated, dont add
+//run through array of prefixes, run through each of its categories through linked list
+//match it to new file - save each link to new hashmap of prefixes
+//run for loop - else if for each prefix - output its linkedlist of crap
+
+//ignore all spaces parse
